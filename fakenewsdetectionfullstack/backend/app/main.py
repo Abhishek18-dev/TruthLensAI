8624747ad2f184ed991dd35bb8e0e1d38f7737e5
Abhishek <bhaidev037@gmail.com>
2026-08-06 00:28:50 +0530

    Update main.py

--- fakenewsdetectionfullstack/backend/app/main.py
+++ fakenewsdetectionfullstack/backend/app/main.py
@@ -37,7 +37,12 @@ app = FastAPI(
 # CORS configurations for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://truth-lens-ai-ten.vercel.app",
+        "https://judicial-aorta-outwit.ngrok-free.dev",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
